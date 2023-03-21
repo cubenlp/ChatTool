17b3a5486578afe776d61a73b6020f15f9e75e2e
rex <1073853456@qq.com>
2023-03-21 23:58:02 +0800

    use markdown instead of restructuredtext

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
     install_requires=requirements,
     license="MIT license",
     long_description=readme,
+    long_description_content_type='text/markdown',
     include_package_data=True,
     keywords='openai_api_call',
     name='openai_api_call',
