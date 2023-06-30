d8763c0b735de3ecf1925cc6ec5e2529ec68e9b4
rex <1073853456@qq.com>
2023-07-01 02:41:59 +0800

    add test from openai

--- openai_api_call/response.py
+++ openai_api_call/response.py
@@ -26,26 +26,6 @@ class Resp():
         return self.content
 
     @property
-    def total_tokens(self):
-        """Total number of tokens"""
-        return self.response['usage']['total_tokens']
-    
-    @property
-    def prompt_tokens(self):
-        """Number of tokens in the prompt"""
-        return self.response['usage']['prompt_tokens']
-    
-    @property
-    def completion_tokens(self):
-        """Number of tokens of the response"""
-        return self.response['usage']['completion_tokens']
-    
-    @property
-    def content(self):
-        """Content of the response"""
-        return self.response['choices'][0]['message']['content']
-    
-    @property
     def id(self):
         return self.response['id']
     
@@ -56,6 +36,36 @@ class Resp():
     @property
     def created(self):
         return self.response['created']
+    
+    @property
+    def usage(self):
+        """Token usage"""
+        return self.response['usage']
+    
+    @property
+    def total_tokens(self):
+        """Total number of tokens"""
+        return self.usage['total_tokens']
+    
+    @property
+    def prompt_tokens(self):
+        """Number of tokens in the prompt"""
+        return self.usage['prompt_tokens']
+    
+    @property
+    def completion_tokens(self):
+        """Number of tokens of the response"""
+        return self.usage['completion_tokens']
+    
+    @property
+    def message(self):
+        """Message"""
+        return self.response['choices'][0]['message']
+    
+    @property
+    def content(self):
+        """Content of the response"""
+        return self.message['content']
     
     def is_valid(self):
         """Check if the response is an error"""
@@ -89,4 +99,5 @@ class Resp():
     @property
     def function_call(self):
         """Function call"""
-        return self.response['choices'][0]['message']['function_call']
+        msg = self.message
+        return None if 'function_call' not in msg else msg['function_call']
