08810bbdc617e0ed4d63124271ed6649a294032a
rex <1073853456@qq.com>
2023-07-02 12:46:40 +0800

    update response wrapper

--- openai_api_call/response.py
+++ openai_api_call/response.py
@@ -1,6 +1,7 @@
 # Response class for OpenAI API call
 
 from typing import Dict
+import json
 
 class Resp():
     
@@ -12,7 +13,7 @@ class Resp():
     def _strip_content(self):
         """Strip the content"""
         self.response['choices'][0]['message']['content'] = \
-            self.response['choices'][0]['message']['content'].strip()
+            self.content.strip()
     
     def __repr__(self) -> str:
         if self.finish_reason == 'stop':
@@ -26,28 +27,12 @@ class Resp():
         return self.content
 
     @property
-    def total_tokens(self):
-        """Total number of tokens"""
-        return self.response['usage']['total_tokens']
-    
-    @property
-    def prompt_tokens(self):
-        """Number of tokens in the prompt"""
-        return self.response['usage']['prompt_tokens']
-    
-    @property
-    def completion_tokens(self):
-        """Number of tokens of the response"""
-        return self.response['usage']['completion_tokens']
-    
-    @property
-    def content(self):
-        """Content of the response"""
-        return self.response['choices'][0]['message']['content']
-    
-    @property
     def id(self):
         return self.response['id']
+    
+    @property
+    def object(self):
+        return self.response['object']
     
     @property
     def model(self):
@@ -57,36 +42,85 @@ class Resp():
     def created(self):
         return self.response['created']
     
+    @property
+    def usage(self):
+        """Token usage"""
+        return self.response['usage']
+    
+    @property
+    def total_tokens(self):
+        """Total number of tokens"""
+        return self.usage['total_tokens']
+    
+    @property
+    def prompt_tokens(self):
+        """Number of tokens in the prompt"""
+        return self.usage['prompt_tokens']
+    
+    @property
+    def completion_tokens(self):
+        """Number of tokens of the response"""
+        return self.usage['completion_tokens']
+    
+    @property
+    def message(self):
+        """Message"""
+        return self.response['choices'][0]['message']
+    
+    @property
+    def content(self):
+        """Content of the response"""
+        return self.message['content']
+
+    @property
+    def finish_reason(self):
+        """Finish reason"""
+        return self.message['finish_reason']
+
+    @property
+    def function_call(self):
+        """Function call"""
+        if self.is_function_call:
+            args = {}
+            args['name'] = self.message['function_call']['name']
+            args['arguments'] = json.loads(self.message['function_call']['arguments'])
+            return args
+        else:
+            return None
+    
+    @property
+    def is_function_call(self):
+        """Check if the response is a function call"""
+        return self.finish_reason == 'function_call' and \
+            self.content is None
+    
     def is_valid(self):
         """Check if the response is an error"""
         return 'error' not in self.response and 'choices' in self.response
     
     @property
+    def error(self):
+        """Error"""
+        return self.response['error']
+    
+    @property
     def error_message(self):
         """Error message"""
-        return self.response['error']['message']
+        return self.error['message']
     
     @property
     def error_type(self):
         """Error type"""
-        return self.response['error']['type']
+        return self.error['type']
     
     @property
     def error_param(self):
         """Error parameter"""
-        return self.response['error']['param']
+        return self.error['param']
     
     @property
     def error_code(self):
         """Error code"""
-        return self.response['error']['code']
+        return self.error['code']
 
-    @property
-    def finish_reason(self):
-        """Finish reason"""
-        return self.response['choices'][0]['finish_reason']
-
-    @property
-    def function_call(self):
-        """Function call"""
-        return self.response['choices'][0]['message']['function_call']
+    
