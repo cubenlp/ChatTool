8f933d74ef3cd2ca2102859190009a04d109e7c5
rex <1073853456@qq.com>
2023-08-31 18:57:39 +0800

    add model property to Chat

--- openai_api_call/__init__.py
+++ openai_api_call/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Rex Wang"""
 __email__ = '1073853456@qq.com'
-__version__ = '1.0.1'
+__version__ = '1.1.0'
 
 import os, requests
 from .chattool import Chat, Resp
--- openai_api_call/chattool.py
+++ openai_api_call/chattool.py
@@ -35,6 +35,7 @@ class Chat():
             raise ValueError("msg should be a list of dict, a string or None")
         self._api_key = openai_api_call.api_key if api_key is None else api_key
         self._chat_url = chat_url
+        self._model = 'gpt-3.5-turbo'
     
     def prompt_token(self, model:str="gpt-3.5-turbo-0613"):
         """Get the prompt token for the model
@@ -46,6 +47,15 @@ class Chat():
             str: prompt token
         """
         return num_tokens_from_messages(self.chat_log, model=model)
+
+    @property
+    def model(self):
+        return self._model
+    
+    @model.setter
+    def model(self, model:str):
+        # assert model.startswith('gpt-'), f'unsupported model {model}'
+        self._model = model
 
     @property
     def api_key(self):
@@ -77,7 +87,7 @@ class Chat():
                    , timeout:int = 0
                    , timeinterval:int = 0
                    , api_key:Union[str, None]=None
-                   , model:str = "gpt-3.5-turbo"
+                   , model:str = None
                    , update:bool = True
                    , **options)->Resp:
         """Get the API response
@@ -96,6 +106,8 @@ class Chat():
         if api_key is None:
             api_key = self.api_key
         assert api_key is not None, "API key is not set!"
+        if model is None:
+            model = self.model
 
         # initialize prompt message
         msg = self.chat_log
