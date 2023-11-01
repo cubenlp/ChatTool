8ee6859f2238af557627b402ad83e9bfdb2f51f3
rex <1073853456@qq.com>
2023-11-01 23:29:31 +0800

    use tqdm for linux windows

--- chattool/asynctool.py
+++ chattool/asynctool.py
@@ -3,7 +3,7 @@ import time, random, warnings, json, os
 from typing import List, Dict, Union, Callable
 from chattool import Chat, Resp, load_chats
 import chattool
-from tqdm.asyncio import tqdm
+import tqdm.asyncio
 
 async def async_post( session
                     , sem
@@ -116,10 +116,10 @@ async def async_process_msgs( chatlogs:List[List[Dict]]
                                  , chatlog=chatlog
                                  , chkpoint=chkpoint
                                  , **options)))
-        if chattool.platform == "macos":
-            responses = await tqdm.gather(tasks)
-        else: # for windows and linux
-            responses = await asyncio.gather(*tasks)
+        try: # for mac or linux
+            responses = await tqdm.asyncio.tqdm.gather(tasks)
+        except TypeError: # for windows or linux | weard bug for linux
+            responses = await tqdm.asyncio.tqdm.gather(*tasks)
         for ind, cost in responses:
             costs[ind] = cost
         return costs
