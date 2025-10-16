a32e753b0e803f6aa54416723e0bb5bbcb530e60
rex <1073853456@qq.com>
2025-10-16 16:22:55 +0800

    update env order

--- src/chattool/const.py
+++ src/chattool/const.py
@@ -14,16 +14,14 @@ CHATTOOL_ENV_FILE = CHATTOOL_CONFIG_DIR / '.env'
 CHATTOOL_REPO_DIR = Path(__file__).parent.parent.parent
 
 # setup environment variables
-# environment > CACHE_DIR/.env file > default values
+# CONFIG_DIR/.env file > environment > default values
 _env_values = dotenv.dotenv_values(CHATTOOL_ENV_FILE)
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY') or _env_values.get('OPENAI_API_KEY')
-OPENAI_API_MODEL = os.getenv('OPENAI_API_MODEL') or _env_values.get('OPENAI_API_MODEL') or 'gpt-3.5-turbo'
-OPENAI_API_BASE_URL = os.getenv('OPENAI_API_BASE_URL') or _env_values.get('OPENAI_API_BASE_URL')
-_api_base = os.getenv('OPENAI_API_BASE')
-if _api_base is None:
-    _api_base = _env_values.get('OPENAI_API_BASE')
-    if _api_base is None and OPENAI_API_BASE_URL:
-        _api_base = OPENAI_API_BASE_URL.rstrip('/') + '/v1'
+OPENAI_API_KEY = _env_values.get('OPENAI_API_KEY') or os.getenv('OPENAI_API_KEY')
+OPENAI_API_MODEL = _env_values.get('OPENAI_API_MODEL') or os.getenv('OPENAI_API_MODEL')  or 'gpt-3.5-turbo'
+OPENAI_API_BASE_URL = _env_values.get('OPENAI_API_BASE_URL') or os.getenv('OPENAI_API_BASE_URL')
+_api_base = _env_values.get('OPENAI_API_BASE') or os.getenv('OPENAI_API_BASE')
+if _api_base is None and OPENAI_API_BASE_URL:
+    _api_base = OPENAI_API_BASE_URL.rstrip('/') + '/v1'
 OPENAI_API_BASE = _api_base or 'https://api.openai.com/v1'
 
 # Azure OpenAI API Key
