598f0c0dd67c3694a3e4c62ab96682513f44e6c6
rexwzh <1073853456@qq.com>
2026-04-07 03:49:47 +0800

    refactor: clean up setup entrypoints

--- src/chattool/setup/cli.py
+++ src/chattool/setup/cli.py
@@ -1,4 +1,29 @@
-from chattool.setup.main import create_setup_group
+import click
+
+from chattool.setup.elements import SETUP_COMMAND_ELEMENTS
+
+
+def _build_setup_command(command_element):
+    callback = command_element.callback
+    for option_element in reversed(command_element.options):
+        decorator = click.argument if option_element.is_argument else click.option
+        callback = decorator(*option_element.param_decls, **option_element.kwargs)(callback)
+    return click.command(name=command_element.name, help=command_element.help)(callback)
+
+
+def register_setup_commands(setup_group):
+    for command_element in SETUP_COMMAND_ELEMENTS:
+        setup_group.add_command(_build_setup_command(command_element))
+    return setup_group
+
+
+def create_setup_group():
+    @click.group(name="setup")
+    def setup_group():
+        """Setup tools for ChatTool."""
+        pass
+
+    return register_setup_commands(setup_group)
 
 
 setup_group = create_setup_group()
